cc6458fc48078f09d2d17c3b0c03282f8ba8fc93
Patrick <pasoit02@hs-esslingen.de>
2017-06-16 11:07:51 +0200

    Audio Player über Bild, da sonst Scroll Problem

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -7,8 +7,8 @@ class MusicPlayer extends React.Component {
     return <div className="musicPlayer">
       <h3>{this.props.musicItem.name}</h3>
       <p>{this.props.musicItem.genre}</p>
-      <div className="PlayerImage"><img src={`${this.props.musicItem.pic_url}`} alt={`${this.props.musicItem.name}`}></img></div>
       <div><audio src={`${this.props.musicItem.url}`} controls></audio></div>
+      <div className="PlayerImage"><img src={`${this.props.musicItem.pic_url}`} alt={`${this.props.musicItem.name}`}></img></div>      
       {/*<div><a href={`http://localhost:3000/play/${this.props.musicItem.url}`}><button><h3>PLAY</h3></button></a></div>*/}
     </div>;
   }
